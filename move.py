0660c9f114fdd9b656247b69e9c88bfb79f76026
Aditya <lapratomo24@yahoo.com>
2024-08-17 21:22:49 +0200

    turtle challenges

---
+++ move.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+for x in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+    timmy.color("red")
+
+
+screen = Screen()
+screen.exitonclick()
