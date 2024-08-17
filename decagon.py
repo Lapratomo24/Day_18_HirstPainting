74a628c97bd60f673bc2d21c202be5f51e335a59
Aditya <lapratomo24@yahoo.com>
2024-08-17 22:16:23 +0200

    refactor with for loop and a method

--- decagon.py
+++ decagon.py
@@ -1,38 +1,21 @@
 from turtle import Turtle, Screen
+import random
 
 tommy = Turtle()
-for x in range (3):
-    tommy.color("orange")
-    tommy.forward(100)
-    tommy.right(120)
-for x in range(4):
-    tommy.color("blue")
-    tommy.forward(100)
-    tommy.right(90)
-for x in range(5):
-    tommy.color("green")
-    tommy.forward(100)
-    tommy.right(72)
-for x in range(6):
-    tommy.color("red")
-    tommy.forward(100)
-    tommy.right(60)
-for x in range(7):
-    tommy.color("pink")
-    tommy.forward(100)
-    tommy.right(51.5)
-for x in range(8):
-    tommy.color("purple")
-    tommy.forward(100)
-    tommy.right(45)
-for x in range(9):
-    tommy.color("black")
-    tommy.forward(100)
-    tommy.right(40)
-for x in range(10):
-    tommy.color("brown")
-    tommy.forward(100)
-    tommy.right(36)
+
+colors = ["Black", "Red", "Gold", "Green", "Orange", "Pink", "Brown", "Blue"]
+
+
+def draw_shape(sides):
+    angle = 360 / sides
+    for _ in range(sides):
+        tommy.forward(100)
+        tommy.right(angle)
+
+
+for x in range(3, 11):
+    tommy.color(random.choice(colors))
+    draw_shape(x)
 
 screen = Screen()
 screen.exitonclick()
